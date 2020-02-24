a2432101926bb41b99a1a0341adae0feb5b7663b
Mayu Imanaka <imamayu.1215@gmail.com>
2020-02-23 21:54:51 -0800

    add:google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,11 +18,17 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `gatsby-starter-default`, ba
         short_name: `starter`,
         start_url: `/`,
         display: `minimal-ui`,
         icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-154911145-2",
       },
     },
     `gatsby-plugin-sass`,
