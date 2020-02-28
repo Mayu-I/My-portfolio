09caec3d9e37c550474c336309fadf2e18667069
Mayu Imanaka <imamayu.1215@gmail.com>
2020-02-27 17:59:06 -0800

    update:file system

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,9 @@ import React from "react"
 import '../styles/styles.scss'
 
 import Header from "../components/Header/header"
-import About from "../components/about"
+import About from "../components/About/about"
 import Works from "../components/Works/works"
-import Contact from "../components/contact"
+import Contact from "../components/Contact/contact"
 import Footer from "../components/Footer/footer"
 
 
